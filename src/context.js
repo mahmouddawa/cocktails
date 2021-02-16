03709bb52163b2fbd8d32a7678717a4f7923173e
Mahmoud Dawa <mahmouddawa990@gmail.com>
2021-02-16 21:12:01 +0100

    finishing the project

--- src/context.js
+++ src/context.js
@@ -9,7 +9,7 @@ const AppProvider = ({ children }) => {
   const [searchTerm, setSearchTerm] = useState('a');
   const [cocktails, setCocktails] = useState([]);
   
-  const fetchDrinks = async ()=>{
+  const fetchDrinks = useCallback(async ()=>{
     setLoading(true)
     try{
       const response = await fetch(`${url}${searchTerm}`);
@@ -38,10 +38,10 @@ const AppProvider = ({ children }) => {
       console.log(err);
       setLoading(false)
     }
-  }
+  },[searchTerm])
   useEffect(()=>{
     fetchDrinks();
-  },[searchTerm]);
+  },[searchTerm, fetchDrinks]);
   return <AppContext.Provider value={{
     loading,
     searchTerm,
